28b2daa99a3000d1c9dd471d3dc07c48534303ff
Kristjan <kristjanfurlan7@gmail.com>
2025-05-12 12:47:23 +0200

    popravki enter

--- calculator/app.py
+++ calculator/app.py
@@ -20,12 +20,12 @@ def evaluate_calculation():
     global calculation
     try:
         calculation = text_result.get(1.0, "end").strip()
-        calculation = str(eval(calculation))
+        calculation = str(round(eval(calculation),2))
         text_result.delete(1.0, "end")
         text_result.insert(1.0, calculation)
     except:
         clear_field()
-        text_result.insert(1.0, "Mi je zelo žal ma nau šlo")
+        text_result.insert(1.0, "Nau šlo")
     pass
 
 def clear_field():
@@ -57,10 +57,17 @@ def handle_opening_bracket():
 
 def validate_keypress(event):
     allowed_keys = "0123456789+-*/()."
+    if event.keysym == "BackSpace":
+        return
+
     if event.char not in allowed_keys:
         return "break" 
     pass
 
+def handle_enter_key(event):
+    evaluate_calculation()
+    return "break"
+    pass
 
 root = tk.Tk()
 root.geometry("300x300")
@@ -70,9 +77,11 @@ root.configure(bg="black")
 
 text_result = tk.Text(root, bg="green", height=2, width=19, font=("Times New Roman", 24))
 text_result.grid(columnspan=5)
-text_result.bind("<Return>", lambda event: evaluate_calculation())
+text_result.bind("<Return>", lambda event: (evaluate_calculation(), "break"))
 text_result.bind("(", lambda event: (handle_opening_bracket(), "break"))
 text_result.bind("<Key>", lambda event: validate_keypress(event))
+text_result.bind("<Return>", handle_enter_key)
+
 
 bt1 = tk.Button(root, text="1", bg="green", command=lambda: add_to_calculation(1), width=5, font=("Arial", 14))
 bt1.grid(row=2, column=0)
